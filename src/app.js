0118ebd217aea29dba9fa0e78af767279244884f
Kevin Bo Huang <42336618+baekjung@users.noreply.github.com>
2018-08-28 14:47:02 +0200

    Add files via upload
    fixed a line for the directory path

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('../public'))
 
-app.get('/helloThere', function(request, response){
-	response.send('Hello there!')
-});
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res){
+//   res.send('hello world');
+// });
 
 app.listen(3000, function(){
 	console.log('working on 3000');
